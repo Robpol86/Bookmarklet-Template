4bf05dfcaa12d98ed7c14129e373753cfa977007
Robpol86 <robpol86@gmail.com>
2026-03-04 14:30:13 +0900

    no class

--- src/entrypoint.mjs
+++ src/entrypoint.mjs
@@ -2,19 +2,18 @@
  * Entrypoint of the bookmarklet.
  */
 
-import { Log } from "./log.mjs";
+import { debug } from "./log.mjs";
 import { main } from "./main.mjs";
 
 /**
  * Entrypoint function of the bookmarklet.
  */
 function entrypoint() {
-    const log = new Log();
-    log.debug(__FNAME_LINENO__, "Start");
+    debug(__FNAME_LINENO__, "Start");
     try {
         return main();
     } finally {
-        log.debug(__FNAME_LINENO__, "End");
+        debug(__FNAME_LINENO__, "End");
     }
 }
 
--- src/log.mjs
+++ src/log.mjs
@@ -1,43 +1,35 @@
 /**
- * Convenience class for logging.
+ * Logging functions.
  */
+
+const PROJECT_KEY = "BOOKMARKLET";
 
 /**
- * Log messages to console with a project key and caller key.
+ * Log an error.
  *
- * The user can filter log messages specific to the bookmarklet using the project key. The caller key is useful to quickly
- * pinpoint where the log message was emitted from. TODO.
+ * @param {string} message - Log message.
+ * @param {...*} args - Additional arguments passed to console.error().
  */
-export class Log {
-    #projectKey = "BOOKMARKLET";
+export function error(message, ...args) {
+    console.error(PROJECT_KEY, message, ...args);
+}
 
-    /**
-     * Log an error.
-     *
-     * @param {string} message - Log message.
-     * @param {...*} args - Additional arguments passed to console.error().
-     */
-    error(message, ...args) {
-        console.error(this.#projectKey, message, ...args);
-    }
+/**
+ * Log an info message.
+ *
+ * @param {string} message - Log message.
+ * @param {...*} args - Additional arguments passed to console.log().
+ */
+export function info(message, ...args) {
+    console.log(PROJECT_KEY, message, ...args);
+}
 
-    /**
-     * Log an info message.
-     *
-     * @param {string} message - Log message.
-     * @param {...*} args - Additional arguments passed to console.log().
-     */
-    info(message, ...args) {
-        console.log(this.#projectKey, message, ...args);
-    }
-
-    /**
-     * Log a debug message.
-     *
-     * @param {string} message - Log message.
-     * @param {...*} args - Additional arguments passed to console.debug().
-     */
-    debug(message, ...args) {
-        console.debug(this.#projectKey, message, ...args);
-    }
+/**
+ * Log a debug message.
+ *
+ * @param {string} message - Log message.
+ * @param {...*} args - Additional arguments passed to console.debug().
+ */
+export function debug(message, ...args) {
+    console.debug(PROJECT_KEY, message, ...args);
 }
